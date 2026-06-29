41f003effa3a607ff1a05f54dfa624a9b0edac30
Chirag Kumar <chiragkumar1718@gmail.com>
2026-06-29 14:47:47 +0530

    similar as second heighest salary with addition of declear m int and set m=n-1 for finding nth salary

---
+++ Nth_heighest_salary.sql
@@ -0,0 +1,11 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+declare m int;
+set m=N-1;
+  RETURN (
+      select(
+        select distinct salary from Employee
+        order by salary desc limit 1 offset m
+      ) 
+  );
+END
